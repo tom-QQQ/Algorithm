f7271db534cb652522cf59cfa261a09104f79b3a
Ning <506868192@qq.com>
2018-07-04 13:14:10 +0800

    git测试

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
 //        regression.verificationResult();
 
-        LogisticRegression logisticRegression = new LogisticRegression(false, true);
+        LogisticRegression logisticRegression = new LogisticRegression(true, true);
         logisticRegression.calculateExampleResult();
 //
 //        logisticRegression.verificationResult();
